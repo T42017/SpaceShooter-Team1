31f9c3548febe8f2b611e86f8ba68050829f31d2
Simon Svensson <simon.svensson@lund.ntig.se>
2017-09-04 10:29:31 +0200

    Added GameObject, Ship and Player classes

---
+++ Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Asteroid_Death_2_Electric_Boogaloo
+{
+    class Player : Ship
+    {
+        public Player(Game game) : base(game)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            LoadTexture("shipPlayer");
+            base.LoadContent();
+        }
+    }
+}
