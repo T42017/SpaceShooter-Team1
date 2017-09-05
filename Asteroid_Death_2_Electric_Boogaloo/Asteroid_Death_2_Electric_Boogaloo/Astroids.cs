8f46ca00beed3b198db2b4fe34f01c336edc3cbc
Marcus Linné <marcus.linne@lund.ntig.se>
2017-09-05 09:15:27 +0200

    future work on the player and soon the movement

--- Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/Astroids.cs
+++ Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/Astroids.cs
@@ -12,7 +12,7 @@ namespace Asteroid_Death_2_Electric_Boogaloo
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         private Texture2D backgroundTexture;
-        private Player player;
+        Player player;
 
         
         public Astroids()
