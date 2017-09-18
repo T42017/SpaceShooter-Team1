7f0d8aeac8869bee9dcf73679037b3bdd31f7324
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-09-18 14:02:50 +0200

    Fixed particles

--- Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/GameObjects/Player.cs
+++ Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/GameObjects/Player.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using Asteroid_Death_2_Electric_Boogaloo.Devices;
 using Asteroid_Death_2_Electric_Boogaloo.Components;
 using Microsoft.Xna.Framework;
@@ -6,7 +8,10 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using  DPSF;
+using ParticleAsteroid_Death_2_Electric_Boogaloo.ParticlesEngine2D;
+using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
 
 namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects
 {
@@ -17,19 +22,28 @@ namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects
         private SoundEffect _pewEffect;
         private DateTime _timeSenceLastShot = DateTime.Today;
         private Texture2D _lifeTexture;
+        ParticleEngine particleEngine;
+        private List<Texture2D> textures = new List<Texture2D>();
         public static int score = 0;
 
         public Player(AsteroidsGame game) : base(game, new Weapon(game, Weapon.Type.Missile, Weapon.Color.Blue))
         {
             Health = 10;
+            
             ShootingSpeed = 200;
+           
         }
         
         public override void LoadContent()
         {
+            textures.Add(Game.Content.Load<Texture2D>("blackSmoke00"));
+            textures.Add(Game.Content.Load<Texture2D>("blackSmoke01"));
+            textures.Add(Game.Content.Load<Texture2D>("blackSmoke02"));
+            textures.Add(Game.Content.Load<Texture2D>("blackSmoke03"));
             Texture = TextureManager.Instance.PlayerShipTexture;
             _lifeTexture = Game.Content.Load<Texture2D>("playerLife2_red");
             _pewEffect = Game.Content.Load<SoundEffect>("Blaster");
+            particleEngine = new ParticleEngine(textures, new Vector2(400, 240));
         }
 
         public override void Update()
@@ -37,7 +51,10 @@ namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects
             var gamePadState = GamePad.GetState(PlayerIndex.One);
             
             KeyboardState state = Keyboard.GetState();
-            
+
+            particleEngine.EmitterLocation = Position;
+            particleEngine.Update();
+
             //Movement using the left, right joystick and the Dpad on the Xbox controller or the arrows or WASD on the keyboard
             if ((gamePadState.ThumbSticks.Left.Y >= 0.3f)
                 || (gamePadState.DPad.Up == ButtonState.Pressed)
@@ -88,6 +105,8 @@ namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            particleEngine.Draw(spriteBatch);
+
             base.Draw(spriteBatch);
 
             //Draw player health
