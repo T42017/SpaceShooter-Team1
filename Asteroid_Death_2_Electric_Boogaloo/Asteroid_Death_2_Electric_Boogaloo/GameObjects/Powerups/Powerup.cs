8b50d72cb57816edcb2509a25b2cd2854bec63e0
Simon Svensson <simon.svensson@lund.ntig.se>
2017-09-21 13:39:24 +0200

    Now mariostar working!!

--- Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/GameObjects/Powerups/Powerup.cs
+++ Asteroid_Death_2_Electric_Boogaloo/Asteroid_Death_2_Electric_Boogaloo/GameObjects/Powerups/Powerup.cs
@@ -6,7 +6,7 @@ namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects.Powerups
     public abstract class Powerup : GameObject
     {
         public PowerupType PowerupType { get; }
-        public int Timer { get; private set; }
+        public int Timer { get; set; }
 
         protected AsteroidsGame Game;
         
@@ -26,11 +26,5 @@ namespace Asteroid_Death_2_Electric_Boogaloo.GameObjects.Powerups
         public abstract void Remove(Player player);
         
         public abstract void DoEffect(Player player);
-
-        public override void Update()
-        {
-            Timer--;
-            base.Update();
-        }
     }
 }
